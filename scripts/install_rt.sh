80fe7f76af941eb65c99793465cd0586841cb453
Nazarii Vitiv <nazarvitiv29@gmail.com>
2022-10-01 12:47:48 +0300

    Reworked script for rt installation.

--- scripts/install_rt.sh
+++ scripts/install_rt.sh
@@ -2,8 +2,26 @@
 
 # for correct install run with sudo
 
-gcc c++_files/rt.c -o rt
-chown root:root rt
-cp rt /usr/bin
-chmod +s /usr/bin/rt
-rm rt
+PATH_TO_SRC=$(find -name rt.c)
+
+compile() {
+    gcc c++_files/rt.c -o rt
+    chown root:root rt
+    cp rt /usr/bin
+    chmod +s /usr/bin/rt
+    rm rt
+}
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root -> (sudo ./scripts/install_rt.sh)"
+  exit
+fi
+
+if [ -z "$PATH_TO_SRC" ]
+then
+    echo "Source file rt.c not found. Try to run script from project directory"
+    exit
+fi
+
+echo "Compilation of rt. Path = $PATH_TO_SRC"
+compile
